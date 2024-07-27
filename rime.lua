5f9123959a974a2120e74f78ae81cc1d5eb53981
OverflowCat <overflowcat@gmail.com>
2024-07-27 20:30:06 +0800

    init: tiger

--- rime.lua
+++ rime.lua
@@ -1,3 +1,4 @@
+-- begin flypy
 function date_translator(input, seg)
    if (input == "orq") then
       --- Candidate(type, start, end, text, comment)
@@ -15,3 +16,15 @@ function time_translator(input, seg)
 end
 
 calculator_translator = require("calculator_translator")
+-- end flypy
+
+-- begin tiger
+charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
+core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+-- dz_ci = require("dz_ci_filter") --单字模式 这个别用，有问题的
+number_translator = require("number")
+lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
+calculator_translator = require("calculator_translator")  --简易计算器
+exe_processor = require("exe")  -- 网页启动器
+shijian2_translator = require("shijian2") -- 高级时间
+-- end tiger
